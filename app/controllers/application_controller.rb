a1140b1f6b2f9ea397ace98209d2af5ed907624e
abbeytipton <abbeyktipton@gmail.com>
2019-09-21 17:01:25 -0500

    added user functionality - create account, log in and out of account, and page greeting you by your input name once logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  # Protection from hackers
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
+  # Helper methods
+  helper_method [:current_user]
+
+  def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
---
+++ db/migrate/20190921212633_add_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :name, :string
+  end
+end
