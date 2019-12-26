c9e03b0272a78ccbce6bbd2e33870dd2651f68d0
abbeytipton <abbeyktipton@gmail.com>
2019-12-26 16:10:48 -0600

    finished adding all graphs, clickable when you click on any other pie piece in the main larger graph

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,6 +40,15 @@ var medicine = $('#medicine').text();
 var medicine_helped = $('#medicine_helped').text() / medicine * 100;
 var medicine_didnt_help = 100 - medicine_helped;
 var no_alcohol = 100 - alcohol;
+var no_chocolate = 100 - chocolate;
+var no_dehydrated = 100 - dehydrated;
+var no_overeating = 100 - overeating;
+var no_period = 100 - period;
+var no_exercise = 100 - exercise;
+var no_eyestrain = 100 - eye_strain;
+var no_lights = 100 - lights;
+var no_sleep = 100 - sleep;
+var no_stress = 100 - stressed;
 
 // All Symptoms chart set up and push to the right div //
 var allSymptoms = {
@@ -66,6 +75,7 @@ var allSymptoms = {
   },
 	data: [{
 		type: "doughnut",
+		explodeOnClick: false,
     percentFormatString: "#",
 		innerRadius: "30%",
 		showInLegend: true,
@@ -76,16 +86,17 @@ var allSymptoms = {
     total: total,
 		indexLabel: "{label}: ",
 		dataPoints: [
-			{ label: "Chocolate", y: chocolate },
+			{ label: "Chocolate", y: chocolate, click: clickChocolate },
       { label: "Alcohol", y: alcohol, click: clickAlcohol},
-      { label: "Stress", y: stressed },
-      { label: "Lack of Sleep", y: sleep },
-      { label: "Bright Lights", y: lights },
-      { label: "Eye Strain", y: eye_strain },
-      { label: "Over Exercising", y: exercise },
-      { label: "During Period", y: period },
-      { label: "Overeating", y: overeating },
-			{ label: "Dehydration", y: dehydrated }
+      { label: "Stress", y: stressed, click: clickStress },
+      { label: "Lack of Sleep", y: sleep, click: clickSleep },
+      { label: "Bright Lights", y: lights, click: clickLights },
+      { label: "Eye Strain", y: eye_strain, click: clickEyestrain },
+      { label: "Over Exercising", y: exercise, click: clickExercise },
+      { label: "During Period", y: period, click: clickPeriod },
+      { label: "Overeating", y: overeating, click: clickOvereating },
+			{ label: "Dehydration", y: dehydrated, click: clickDehydrated },
+			{ label: "Medicine", y: medicine_helped, click: clickMedicine }
 		]
 	}]
 };
@@ -176,10 +187,458 @@ var alcoholGraph = {
 };
 $("#alcoholSymptoms").CanvasJSChart(alcoholGraph);
 
+// Chocolate chart set up and push to the right div //
+var chocolateGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Had Chocolate Before the Headache", y: chocolate },
+			{ label: "You Didn't Have Chocolate Before the Headache", y: no_chocolate }
+		]
+	}]
+};
+$("#chocolateSymptoms").CanvasJSChart(chocolateGraph);
+
+// Dehydrated chart set up and push to the right div //
+var dehydratedGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Were Dehydrated Before the Headache", y: dehydrated },
+			{ label: "You Weren't Dehydrated Before the Headache", y: no_dehydrated }
+		]
+	}]
+};
+$("#dehydratedSymptoms").CanvasJSChart(dehydratedGraph);
+
+// Overeating chart set up and push to the right div //
+var overeatingGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Were Overeating Before the Headache", y: overeating },
+			{ label: "You Weren't Overeating Before the Headache", y: no_overeating }
+		]
+	}]
+};
+$("#overeatingSymptoms").CanvasJSChart(overeatingGraph);
+
+// Period chart set up and push to the right div //
+var periodGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Were on Your Period Before/During the Headache", y: period },
+			{ label: "You Weren't on Your Period Before/During the Headache", y: no_period }
+		]
+	}]
+};
+$("#periodSymptoms").CanvasJSChart(periodGraph);
+
+// Exercise chart set up and push to the right div //
+var exerciseGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Were Over Excercising Before the Headache", y: exercise },
+			{ label: "You Weren't Over Exercising Before the Headache", y: no_exercise }
+		]
+	}]
+};
+$("#exerciseSymptoms").CanvasJSChart(exerciseGraph);
+
+// Eyestrain chart set up and push to the right div //
+var eyestrainGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Experienced Eyestrain Before the Headache", y: eye_strain },
+			{ label: "You Didnt't Experience Eyestrain Before the Headache", y: no_eyestrain }
+		]
+	}]
+};
+$("#eyestrainSymptoms").CanvasJSChart(eyestrainGraph);
+
+// Lights chart set up and push to the right div //
+var lightsGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Experienced Bright Lights Before the Headache", y: lights },
+			{ label: "You Didnt't Experience Bright Lights Before the Headache", y: no_lights }
+		]
+	}]
+};
+$("#lightsSymptoms").CanvasJSChart(lightsGraph);
+
+// Sleep chart set up and push to the right div //
+var sleepGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Experienced Lack of Sleep Before the Headache", y: sleep },
+			{ label: "You Didnt't Experience Lack of Sleep Before the Headache", y: no_sleep }
+		]
+	}]
+};
+$("#sleepSymptoms").CanvasJSChart(sleepGraph);
+
+// Stress chart set up and push to the right div //
+var stressGraph = {
+
+	animationEnabled: true,
+  backgroundColor: "transparent",
+
+	title: {
+		text: ""
+	},
+	width:500,
+	height: 400,
+
+  legend: {
+		maxWidth: 350,
+		itemWidth: 120
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false
+	},
+  toolTip:{
+    enabled: false,
+  },
+	data: [{
+		type: "doughnut",
+    percentFormatString: "#",
+		innerRadius: "30%",
+		showInLegend: true,
+		legendText: "{label}",
+    indexLabelFormatter: function(e){
+				return e.dataPoint.label + " " + Math.round(e.dataPoint.y) + "%";
+			},
+    total: total,
+		indexLabel: "{label}: ",
+		dataPoints: [
+			{ label: "You Experienced Stress Before the Headache", y: stressed },
+			{ label: "You Didnt't Experience Stress Before the Headache", y: no_stress }
+		]
+	}]
+};
+$("#stressSymptoms").CanvasJSChart(stressGraph);
+
 // Click functions for each piece of the all triggers pie //
 function clickAlcohol(e) {
-	$("#triggerHolder").fadeOut(function () {
+	$('.children').fadeOut().promise().done(function () {
     $("#alcoholHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickChocolate(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#chocolateHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickDehydrated(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#dehydratedHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickOvereating(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#overeatingHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickPeriod(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#periodHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickExercise(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#exerciseHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickEyestrain(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#eyestrainHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickLights(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#lightsHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickSleep(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#sleepHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickStress(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#stressHolder").fadeIn(1000);
+		$("#triggerHolder").fadeIn(1000);
+});
+};
+
+function clickMedicine(e) {
+	$('.children').fadeOut().promise().done(function () {
+    $("#medicineHolder").fadeIn(1000);
 		$("#triggerHolder").fadeIn(1000);
 });
 };
