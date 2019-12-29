936d49506fb11087b0f25d2122dde54c8ec234ec
abbeytipton <abbeyktipton@gmail.com>
2019-12-29 08:54:02 -0600

    precompile assets

--- db/migrate/20190921213920_add_username_to_users.rb
+++ db/migrate/20190921213920_add_username_to_users.rb
@@ -1,5 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :username, :string
+    add_column :users, :string
+    add_column :username, :string
   end
 end
