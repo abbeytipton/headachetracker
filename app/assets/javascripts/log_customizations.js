d521bae78c7ae709f3a2d143b8a2219c4a34ff7b
abbeytipton <abbeyktipton@gmail.com>
2020-01-19 16:11:33 -0600

    working on create method for customizing triggers questions

--- app/assets/javascripts/log_customizations.js
+++ app/assets/javascripts/log_customizations.js
@@ -1,9 +1,1 @@
 // Click functions for the next buttons that make the logging form visible item by item to the user //
-
-// When the one next button is clicked, start this function //
-$(document).on("click", "#startNext", function(e){
-    // Fade out the first div and fade in the second //
-    $("#start").fadeOut(function() {
-    //$("#one").fadeIn(1500);
-});
-});
