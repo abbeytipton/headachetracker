66d85217dcc1413f2186d89c035ced5281610b6b
abbeytipton <abbeyktipton@gmail.com>
2020-02-11 19:50:31 -0600

    working on turbolinks issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require activestorage
-//= require turbolinks
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
