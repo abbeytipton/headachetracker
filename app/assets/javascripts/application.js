3eb5da3e5b0bd85122e70ad4c1f1f62444e4fd47
abbeytipton <abbeyktipton@gmail.com>
2020-02-16 15:51:45 -0600

    working on js for customization page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -710,7 +710,8 @@ $(document).on("click", "#twelveNextNo", function(e){
 });
 
 // When the thirteen yes button is clicked, start this function //
-$(document).on("click", "#thirteenNextYes", function(e){
+//$(document).on("click", "#thirteenNextYes", function(e){
+  $('#thirteenNextYes').click(function () {
      if ($('#trigger1NameText').val() == "") {
        alert("hey");
      }
