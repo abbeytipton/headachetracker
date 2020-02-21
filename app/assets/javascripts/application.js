58484fefd663a2b9f0dcd7a5660323975883ca1d
abbeytipton <abbeyktipton@gmail.com>
2020-02-21 15:39:28 -0600

    rewriting js for log customization

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -614,7 +614,7 @@ $(document).on("click", "#nextYes, #nextNo", function(e){
   var checkboxToShow = erbCheckboxes[counter];
   $("#checkboxDiv").append(checkboxToShow);
   $("#question").empty();
-  $("#question").append(questions[counter]);
+  $('#question').html($(questions[counter])).html());
   counter++;
 });
 
