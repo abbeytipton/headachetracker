afca02e2c6bf5eca17e50e68a2bedfb7e52cdb74
abbeytipton <abbeyktipton@gmail.com>
2020-04-05 16:32:06 -0500

    polishing up log customization page

--- app/controllers/log_customization_controller.rb
+++ app/controllers/log_customization_controller.rb
@@ -6,7 +6,7 @@ class LogCustomizationController < ApplicationController
 
 # Defines what new log is and saves
   def create
-    @logcustomization = LogCustomization.new(logcustomizations_params)
+    @logcustomization = LogCustomization.new(logcustomization_params)
     if @logcustomization.save
         redirect_to '/logs/index'
       else
@@ -18,7 +18,7 @@ class LogCustomizationController < ApplicationController
 
   # Defines params needed for product to be set up, also allows teacher_id to come through from hidden field
   def logcustomizations_params
-    params.require(:log_customizations).permit(:userID, :trigger1, :trigger1Name, :trigger2, :trigger2Name,
+    params.require(:logcustomization).permit(:userID, :trigger1, :trigger1Name, :trigger2, :trigger2Name,
                                 :trigger3, :trigger3Name, :trigger4, :trigger4Name, :trigger5, :trigger5Name,
                                 :trigger6, :trigger6Name, :trigger7, :trigger7Name, :trigger8, :trigger8Name, :trigger9,
                                 :trigger9Name, :trigger10, :trigger10Name, :alcohol, :overeating, :stress, :sleep,
