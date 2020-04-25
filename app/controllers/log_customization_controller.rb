ab50b45b7dc55a5c60e83992df631fe10d1540d8
abbeytipton <abbeyktipton@gmail.com>
2020-04-25 15:20:33 -0500

    working on this param issue for log custom.:

--- app/controllers/log_customization_controller.rb
+++ app/controllers/log_customization_controller.rb
@@ -46,7 +46,7 @@ class LogCustomizationController < ApplicationController
 
   # Defines params needed for product to be set up, also allows teacher_id to come through from hidden field
   def logcustomization_params
-    params.require(:log_customizations).permit(:userID, :trigger1, :trigger1Name, :trigger2, :trigger2Name,
+    params.require(:log_customization).permit(:userID, :trigger1, :trigger1Name, :trigger2, :trigger2Name,
                                 :trigger3, :trigger3Name, :trigger4, :trigger4Name, :trigger5, :trigger5Name,
                                 :trigger6, :trigger6Name, :trigger7, :trigger7Name, :trigger8, :trigger8Name, :trigger9,
                                 :trigger9Name, :trigger10, :trigger10Name, :alcohol, :overeating, :stress, :sleep,
