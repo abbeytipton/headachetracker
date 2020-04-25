16d211f20a03a8d163c6c64491245107b6264e97
abbeytipton <abbeyktipton@gmail.com>
2020-04-25 15:33:06 -0500

    working on this param issue for log custom.:

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   # Log custom routes
   resources :log_customization, only: [:new, :create]
-  get 'log_customization/create', :to => 'log_customization#new'
+  post 'log_customization/create', :to => 'log_customization#new'
 
   # Graphs routes
   get 'graphs/index'
