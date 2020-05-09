5d91b9772d885b284a5a0d11ac5cb022dd289e29
abbeytipton <abbeyktipton@gmail.com>
2020-05-09 16:52:38 -0500

    working on adding custom triggers to log page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -100,7 +100,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_trigger1
-    @user_trigger1 ||= LogCustomization.select(:trigger1Name).find_by(:userID => current_user.id)
+    @user_trigger1 ||= LogCustomization.where(:userID => current_user.id).pluck(:trigger1Name)
   end
 
   # Get the total number of each symptom for the current user
