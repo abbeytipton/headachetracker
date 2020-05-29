6171fc6dd03226dc291c4d59e24180e40d85d339
abbeytipton <abbeyktipton@gmail.com>
2020-05-29 14:56:24 -0500

    working on edit page for logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,8 @@
 class LogsController < ApplicationController
-
-  # Require user login before these pages can be viewed #
+  before_action :set_log, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:index]
-
-
+  # GET /logs
+  # GET /logs.json
   def index
     if customized == 0
       redirect_to '/log_customization/create'
@@ -11,55 +10,70 @@ class LogsController < ApplicationController
     end
   end
 
-  # Creates new log instance
-  def new
-    @logs = Logs.new
-  end
-
+  # GET /logs/1
+  # GET /logs/1.json
   def show
   end
 
-# Defines what new log is and saves
+  # GET /logs/new
+  def new
+    @log = Log.new
+  end
+
+  # GET /logs/1/edit
+  def edit
+  end
+
+  # POST /logs
+  # POST /logs.json
   def create
-    @logs = Logs.new(logs_params)
-    if @logs.save
+    @log = Log.new(log_params)
+
+    respond_to do |format|
+      if @log.save
         redirect_to '/graphs/index'
         flash[:notice] = " Your log has been added successfully. Check out your updated graphs below. "
       else
-        redirect_to '/logs/index'
+        format.html { render :new }
+        format.json { render json: @log.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-# Deletes a product
-  def destroy
-    Logs.find(params[:id]).destroy
-    flash[:delete] = " Log deleted successfully! "
-    redirect_to '/logs/index'
-  end
-
-# Edits a product
-  def edit
-    @logs = Logs.find(params[:id])
-  end
-
-# Updates a product based on user input
+  # PATCH/PUT /logs/1
+  # PATCH/PUT /logs/1.json
   def update
-    @logs = Logs.find(params[:id])
-    if @logs.update(logs_params)
-      redirect_to '/logs/index'
-      flash[:notice] = " Log updated successfully! "
-    else
-      redirect_to '/logs/index'
+    respond_to do |format|
+      if @log.update(log_params)
+        format.html { redirect_to @log, notice: 'Log was successfully updated.' }
+        format.json { render :show, status: :ok, location: @log }
+      else
+        format.html { render :edit }
+        format.json { render json: @log.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /logs/1
+  # DELETE /logs/1.json
+  def destroy
+    @log.destroy
+    respond_to do |format|
+      format.html { redirect_to logs_url, notice: 'Log was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_log
+      @log = Log.find(params[:id])
+    end
 
-  # Defines params needed for product to be set up, also allows teacher_id to come through from hidden field
-  def logs_params
-    params.require(:logs).permit(:userid, :date_started, :date_ended, :period, :dehydration,
-                                :exercise, :eye_strain, :lights, :sleep, :stress, :overeating,
-                                :medicine, :medicine_helped, :weather, :alcohol, :chocolate, :custom1, :custom2, :custom3, :custom4, :custom5)
-  end
-
+    # Only allow a list of trusted parameters through.
+    def logs_params
+      params.require(:logs).permit(:userid, :date_started, :date_ended, :period, :dehydration,
+                                  :exercise, :eye_strain, :lights, :sleep, :stress, :overeating,
+                                  :medicine, :medicine_helped, :weather, :alcohol, :chocolate, :custom1, :custom2, :custom3, :custom4, :custom5)
+    end
 end
