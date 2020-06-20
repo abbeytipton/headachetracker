61801521faea796798f42df66f8e23eddade6189
abbeytipton <abbeyktipton@gmail.com>
2020-06-19 19:01:55 -0500

    working on edit log page

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,7 @@ Rails.application.routes.draw do
   # Logs routes
   get 'logs/index'
   resources :logs, only: [:show, :edit, :update]
-  get 'logs/edit'
   get 'newlog', to: 'logs#new', as: 'newlog'
-
-  resources :logs do
-    resources :edit
-end
 
 
   # Log custom routes
