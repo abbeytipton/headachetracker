cc41afccb42595ae0d1907e4a344465f0c613ce7
abbeytipton <abbeyktipton@gmail.com>
2020-07-22 15:01:27 -0500

    adding instructions to the graphs page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,34 @@
 
 $(document).on('turbolinks:load', function() {
 
+  // Get the modal
+var modal = document.getElementById("myModal");
+var dropdown = document.getElementById("dropdownMenu");
+
+// Get the button that opens the modal
+var btn = document.getElementById("instructionsClick");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    dropdown.style.display = "none";
+  }
+}
+
   // Get each trigger //
   var total = $('#total').text();
   var chocolate = Math.floor($('#chocolate').text() / total * 100);
@@ -208,34 +236,6 @@ $(document).on('turbolinks:load', function() {
       // Toggle the dropdown menu button to show/hide the menu when the button is clicked //
       $(document).on("click", "#moreClick", function(e){
         $("#dropdownMenu").toggle();
-      });
-
-      // Modal functions for the instructions and total headaches links //
-      // Get the modal
-      var modal = document.getElementById("Modal");
-
-      // Get the p inside the modal for text to be placed in //
-      var p = document.getElementById("pInsideModal");
-
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-        modal.style.display = "none";
-      }
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-
-      // Click function for instructions link from drop down menu //
-      $(document).on("click", "#totalClick", function(e){
-        modal.style.display = "block";
-        p.innerHTML = "You have logged " + total + " headaches so far.";
       });
   }
 
