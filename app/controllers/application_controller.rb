1eb607a1c4d19d26d3f5b5d0431605433060c1ad
abbeytipton <abbeyktipton@gmail.com>
2020-08-09 16:01:06 -0500

    working on trigger edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_triggers
-    @user_triggers = LogCustomization.where(:userID => current_user.id).first.pluck(:id).flatten.join(' ')
+    @user_triggers = LogCustomization.where(:userID => current_user.id).first.id
   end
 
   # Get the total number of headaches logged by this user
