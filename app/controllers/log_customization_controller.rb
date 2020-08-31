ca6ec9f778420f5fedbb7cba5c71983f191b0c65
abbeytipton <abbeyktipton@gmail.com>
2020-08-30 19:20:45 -0500

    fixing custom trigger update blank trigger names

--- app/controllers/log_customization_controller.rb
+++ app/controllers/log_customization_controller.rb
@@ -33,7 +33,8 @@ class LogCustomizationController < ApplicationController
 
   def update
     @logcustomization = LogCustomization.find(params[:id])
-    if params[:trigger1Name].blank? && params[:trigger1] == 1
+    if params[:trigger1Name].blank?
+      puts params[:trigger1]
     redirect_to '/graphs/index'
     flash[:notice] = " You didn't enter any info idiot"
   else
